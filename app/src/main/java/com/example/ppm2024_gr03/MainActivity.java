accd9bcddfadb807ff498228824c550ac5f47ae6
elonekrasniqi <elonakrasniqii02@gmail.com>
2024-11-16 21:31:16 +0100

    updated the style

--- app/src/main/java/com/example/ppm2024_gr03/MainActivity.java
+++ app/src/main/java/com/example/ppm2024_gr03/MainActivity.java
@@ -1,30 +1,30 @@
 package com.example.ppm2024_gr03;
 
 import android.content.Intent;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        Handler handler=new Handler();
+
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.icon);
+
+        Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(MainActivity.this, LogIn.class));
                 finish();
             }
-        },4000);
+        }, 4000);
     }
 }
